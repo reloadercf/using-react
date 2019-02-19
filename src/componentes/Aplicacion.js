79ee75457b26dad1235e4774b5a80a90114559ef
reloadercfm <elcfm1@gmail.com>
2019-02-19 14:04:25 -0600

    state

--- src/componentes/Aplicacion.js
+++ src/componentes/Aplicacion.js
@@ -3,22 +3,49 @@ import Productos from './Productos'
 import Footer from './Footer'
 import Header from './Header'
 
+
 class Aplicacion extends Component{
+    // constructor(){
+    //     super();
+    //     this.state={
+    //         productos :[
+    //             {nombre:'libros',precio:300},
+    //             {nombre:'disco de music',precio:100},
+    //             {nombre:'instrumento musical',precio:800},
+    //             {nombre:'reproductor musical',precio:150},
+    //             {nombre:'Album Edicion especial',precio:500}
+    //         ]
+    //     };
+    // }
+    
+    //hay dos formas de hacer states
+        state={
+            productos :[]
+        };
+        componentDidMount(){
+            setTimeout(()=>{
+                //variable con productos
+                const productos=[
+                    {nombre:'libros',precio:300},
+                    {nombre:'disco de music',precio:100},
+                    {nombre:'instrumento musical',precio:800},
+                    {nombre:'reproductor musical',precio:150},
+                    {nombre:'Album Edicion especial',precio:500}
+                ]
+                this.setState({
+                    productos:productos
+                });
+            },3000)
+        }
+
     render(){
-        const productos =[
-            {nombre:'libro',precio:200},
-            {nombre:'disco de music',precio:100},
-            {nombre:'instrumento musical',precio:800},
-            {nombre:'reproductor musical',precio:150},
-            {nombre:'Album Edicion especial',precio:500}
-        ]
         return(
             <div>
                 <Header 
                     titulo='Nuestra Tienda Virtual'
                 />
                 <Productos 
-                productos={productos}
+                    productos={this.state.productos}
                 />
                 <Footer />
             </div>
