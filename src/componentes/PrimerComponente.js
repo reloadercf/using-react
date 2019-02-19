459e1a10b36b6eecf767fb2825c6ce3fdecfbea9
reloadercfm <elcfm1@gmail.com>
2019-02-19 12:40:07 -0600

    my first component

---
+++ src/componentes/PrimerComponente.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class PrimerComponente extends React.Component{
+    render(){
+        return <p>From my first Component</p>
+    }
+}
+export default PrimerComponente;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
 import * as serviceWorker from './serviceWorker';
+import PrimerComponente from './componentes/PrimerComponente';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PrimerComponente />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
