fa2f878860f210bafc2fd10e86f8512486abb801
reloadercfm <elcfm1@gmail.com>
2019-02-19 13:26:13 -0600

    uso de props

--- src/componentes/Aplicacion.js
+++ src/componentes/Aplicacion.js
@@ -5,10 +5,21 @@ import Header from './Header'
 
 class Aplicacion extends Component{
     render(){
+        const productos =[
+            {nombre:'libro',precio:200},
+            {nombre:'disco de music',precio:100},
+            {nombre:'instrumento musical',precio:800},
+            {nombre:'reproductor musical',precio:150},
+            {nombre:'Album Edicion especial',precio:500}
+        ]
         return(
             <div>
-                <Header />
-                <Productos />
+                <Header 
+                    titulo='Nuestra Tienda Virtual'
+                />
+                <Productos 
+                productos={productos}
+                />
                 <Footer />
             </div>
         );
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Header =()=>{
+const Header =(props)=>{
     return(
         <header>
-           <h1>Tienda virtual</h1>
+           <h1>{props.titulo}</h1>
         </header>
     )
 }
